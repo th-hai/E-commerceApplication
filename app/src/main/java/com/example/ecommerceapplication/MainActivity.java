c7e46ec437a3e9d9a8f419670352a32aeb7c4729
Thanh Hai <thanhhai.9920@gmail.com>
2020-07-08 18:23:25 +0700

    Add items on recyclerview

--- app/src/main/java/com/example/ecommerceapplication/MainActivity.java
+++ app/src/main/java/com/example/ecommerceapplication/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.ecommerceapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.ecommerceapplication.Adapters.PlateAdapter;
+import com.example.ecommerceapplication.Models.PlateModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<PlateModel> plateModelList;
+    private PlateAdapter plateAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide Status Bar
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setKeepScreenOn(true);
+        recyclerView.setHasFixedSize(true);
+
+        plateModelList = new ArrayList<>();
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+        plateModelList.add(new PlateModel(R.drawable.one));
+
+        plateAdapter = new PlateAdapter(plateModelList, this);
+        recyclerView.setAdapter(plateAdapter);
+        plateAdapter.notifyDataSetChanged();
+
+        autoScroll();
+    }
+
+    public void autoScroll() {
+        final int speedScroll = 0;
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            int count = 0;
+            @Override
+            public void run() {
+                if(count == plateAdapter.getItemCount())
+                    count = 0;
+                if(count < plateAdapter.getItemCount()){
+                    recyclerView.smoothScrollToPosition(++count);
+                    handler.postDelayed(this, speedScroll);
+                }
+            }
+        };
+        handler.postDelayed(runnable, speedScroll);
     }
 }
---
+++ app/src/main/java/com/example/ecommerceapplication/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.ecommerceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        // Add handler start
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+}
